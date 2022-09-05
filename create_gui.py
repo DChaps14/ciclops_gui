382b823d640031c14cc18d2b8d5135faba0bb210
Dan Chapman <djc244@uclive.ac.nz>
2022-09-05 12:58:53 +1200

    Constructed a GUI using tkinter, and wrote a file to pass the images to the GUI and save the usable ones

---
+++ create_gui.py
@@ -0,0 +1,105 @@
+import tkinter as tk
+from PIL import Image, ImageTk
+import tensorflow as tf
+import matplotlib.pyplot as plt
+import io
+import numpy as np
+
+# Called once per set of crops on an image - alternatively, could call with a list of lists of crops, and once a list has been exhausted, construct the full image from it
+class GUI:
+    
+    def __init__(self, images, masks, info, labels):
+        self.window = tk.Tk()
+        self.checking_mask = False
+        self.images = images
+        self.masks = masks
+        self.crop_info = info
+        self.labels = labels
+        self.current_index = 0
+        self.check_crop_str = "Is the image below a suitable bounding box around one or more '{crop_class}'?"
+        self.check_mask_str = "Are all trainable classes suitably segmented in the image below?"
+        self.mask_alpha = round(255*0.4)
+        
+        # Stores the usable images
+        self.usable_crops = []
+        self.usable_images = []
+        self.usable_masks = []
+        
+        # Components for the GUI
+        self.instruction_label = None
+        self.image_label = None
+        self.confirm_button = None
+        self.reject_button = None
+        
+    def process_images(self, images):
+        """Converts the PIL image to a matplotlib plot, then back to a PIL image to provide a nicely-resized version of the image
+        Applies any masks that are supplied alongside it on top of the original image"""
+        fig = plt.figure()
+        plt.imshow(images[0])
+        for index in range(1, len(images)):
+            plt.imshow(images[index], alpha=0.4)
+        plt.axis('off')
+        
+        img_buf = io.BytesIO()
+        fig.savefig(img_buf, format='png')
+        image = Image.open(img_buf)
+                    
+        new_image = ImageTk.PhotoImage(image)
+        img_buf.close()
+        
+        return new_image
+        
+    
+    def confirm_image(self):
+        print("Confirmed")
+        if self.checking_mask:
+            self.checking_mask = False
+            self.usable_images.append(self.images[self.current_index])
+            self.usable_masks.append(self.masks[self.current_index])
+            self.move_to_next_crop()
+        else:
+            self.checking_mask = True
+            crop_info = self.crop_info[self.current_index]
+            self.usable_crops.append(crop_info)
+            self.instruction_label.configure(text = self.check_mask_str)
+            mask = self.masks[self.current_index] 
+            image = self.images[self.current_index]
+            image_with_mask = self.process_images([image, mask])
+            
+            self.image_label.configure(image = image_with_mask)
+            self.image_label.image = image_with_mask
+        
+        
+    def move_to_next_crop(self):
+        self.current_index += 1
+        if self.current_index >= len(self.images):
+            self.window.quit()
+        else:
+            self.instruction_label.configure(text = self.check_crop_str.format(crop_class = self.labels[self.current_index]))
+            
+            image = self.process_images([self.images[self.current_index]])
+            self.image_label.configure(image = image)
+            self.image_label.image = image
+    
+    def construct_gui(self):
+        image = self.images[self.current_index]
+        if self.crop_info is not None:
+            self.instruction_label = tk.Label(text=self.check_crop_str.format(crop_class = self.labels[self.current_index]))
+            image = self.process_images([image])
+            self.image_label = tk.Label(image = image)
+        else:
+            # We're checking the suitability of the full image
+            self.instruction_label = tk.Label(text=self.check_mask_str)
+            self.checking_mask = True
+            mask = self.masks[self.current_index]
+
+            image_with_mask = self.process_images([image, mask])            
+            self.image_label = tk.Label(image = image_with_mask)          
+            
+        self.confirm_button = tk.Button(self.window, text="Suitable", command=self.confirm_image)
+        self.reject_button = tk.Button(self.window, text="Not Suitable", command=self.move_to_next_crop)
+        self.instruction_label.pack()
+        self.image_label.pack()
+        self.confirm_button.pack(side=tk.LEFT)
+        self.reject_button.pack(side=tk.RIGHT)
+        tk.mainloop()
---
+++ execute_gui.py
@@ -0,0 +1,114 @@
+# Extracting images and masks from the saved files
+import json
+from create_gui import GUI
+import random
+import os
+from PIL import Image
+import tensorflow as tf
+import numpy as np
+import matplotlib.pyplot as plt
+
+BASE_DIR = "UNetPredictions/detections/"
+IMAGE_DIR = BASE_DIR + "images"
+MASK_DIR = BASE_DIR + "masks"
+
+RESULT_DIR = "UNetPredictions/usableImages/"
+try:
+    os.mkdir(RESULT_DIR)
+    os.mkdir(RESULT_DIR + "images")
+    os.mkdir(RESULT_DIR + "masks")
+except:
+    print("Already Created")
+
+for image in os.scandir(IMAGE_DIR):
+  # Currently displaying the crop and the mask on top of it - this could help to confirm whether the cropped image is suitable, as well as the segmentation
+    base_image = None
+    crops = []
+    masks = []
+    crops_info = []
+    labels = []
+    for crop in os.scandir(image.path):
+        if crop.is_dir():
+            continue
+        if crop.name == "base_image.jpg":
+            base_image = crop
+            continue
+        
+        crop_image = Image.open(crop.path)
+        #if not user_accepts_crop(crop_image): # Send the crop to be approved by the user
+            #print("crop not accepted")
+            #continue
+
+        crop_mask = np.load(crop.path.replace("images", "masks").replace(".jpg", ".npy"))
+        with open(crop.path.replace("images", "masks").replace(".jpg", ".txt"), 'r') as crop_info:
+            coords = crop_info.readline().split(" ")
+        
+        crops.append(crop_image)
+        masks.append(crop_mask)
+        crops_info.append(coords)
+        if random.random() < 0.5:
+            labels.append("dog")
+        else:
+            labels.append("cat")
+    
+    print("Launching GUI")
+
+    gui = GUI(crops, masks, crops_info, labels)
+    gui.construct_gui()
+    gui.window.destroy()
+    usable_images = gui.usable_images
+    usable_masks = gui.usable_masks
+    usable_crops = gui.usable_crops
+    
+    if len(usable_masks) == len(masks):
+        base_image = Image.open(base_image.path)
+        image_dims = np.array(base_image).shape
+        base_image_mask = np.zeros((image_dims[0], image_dims[1], 1))
+        for index, mask in enumerate(usable_masks):
+            mask = np.array(mask)
+            mask = np.reshape(mask, (len(mask), len(mask[0]), 1))
+            x1,y1,x2,y2 = usable_crops[index]
+            mask_pad = tf.constant([[int(y1), image_dims[0]-int(y2)], [int(x1), image_dims[1]-int(x2)], [0,0]])
+            resized_mask = tf.pad(mask, mask_pad, "CONSTANT")
+            base_image_mask = np.where(resized_mask, resized_mask, base_image_mask)
+            
+        print("Checking full image")
+            
+        full_image_gui = GUI([base_image], [base_image_mask], None, None)
+        full_image_gui.construct_gui()
+        full_image_gui.window.destroy()
+        full_inaccurate = not full_image_gui.usable_masks
+    else:
+        full_inaccurate = True
+
+    #if not user_accepts_mask(base_image, base_image_mask):
+        #print("user declined the full image as innacurate - there are missing objects, or the instances are masked wrong")
+        #full_inaccurate = True
+    #else:
+        #print("User accepted the full image as accurate")
+        #full_inaccurate = False
+        ## usable_crops.append(base_image)
+        # usable_masks.append(base_image_mask)
+        # usable_crops_info.append([0, 0, image_dims[1], image_dims[0]])
+
+    detections = []
+    for index, crop in enumerate(usable_images):
+        # crop.save(RESULT_DIR + f"images/{image.name}_{index}.jpg")
+        crop_info = usable_crops[index]
+
+        # Rework the mask to only store the elements within the bounding box
+        crop_mask = usable_masks[index]
+        crop_mask_list = np.ndarray.tolist(np.array(crop_mask))
+        detections.append({ "mask": crop_mask_list, "bounding_box": [crop_info[0], crop_info[1], crop_info[2], crop_info[3]] }) # Stored in xyxy format
+
+    if detections:
+        crop_dict = {
+                      "filename": f"{image.name}.jpg", 
+                      "ground_truth": {"detections": detections},
+                      "skip_full_mask": full_inaccurate #Signifies whether the detections in ground_truth can be sed to formulate a full image to feed into the model
+                    }
+        with open(RESULT_DIR + f'masks/{image.name}.json', 'w') as mask_file:
+            json.dump(crop_dict, mask_file)
+        base_image.save(RESULT_DIR + f"images/{image.name}.jpg") # Reader can use the bounding values to extract the cropped image from the base image for training
+    
+
